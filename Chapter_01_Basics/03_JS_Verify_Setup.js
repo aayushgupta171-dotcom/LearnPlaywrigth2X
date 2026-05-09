e2a909d5601faa8fc9ec74acc62e01d299323ec4
aayushgupta171-dotcom <aayushgupta171@gmail.com>
2026-05-09 17:05:52 +0530

    javascipt learn

---
+++ Chapter_01_Basics/03_JS_Verify_Setup.js
@@ -0,0 +1,11 @@
+console.log(process.platform);
+
+
+// WINDOWS - WIN32
+
+console.log(process.arch);
+// x64
+
+
+console.log("Node Version:", process.version);
+///Node Version: v24.15.0
---
+++ Chapter_03_identifiers_Literals/08_Comments.js
@@ -0,0 +1,37 @@
+// This is sinle comment this will be ignore 
+// this line will be not executed
+
+
+/*
+ *  This is multi line
+ *  Author : Ayush Kumar
+ *  Date : 01-Feb-2000
+ */
+
+/**
+ *  This is multi line
+ *  Author : Ayush Kumar
+ *  Date : 01-Feb-2000
+ **/
+
+// var g = 10;
+
+// this Ayush
+// this Ayush
+// this Ayush
+// this Ayush
+// this              Ayush
+// this Ayush
+// this Ayush
+//          this Ayush
+// this Ayush
+// this Ayush
+//           this Ayush
+// this Ayush
+// this         Ayush
+// this Ayush
+// this Ayush
+// this          Ayush
+// this Ayush
+
+var a = 10
